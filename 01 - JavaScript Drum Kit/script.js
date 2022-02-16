4719914661de1f40a51ae291b58f16d81bc62698
deliags <deliagsmith@outlook.com>
2022-02-16 20:48:44 +0000

    Added function to remove the transition effects

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -4,18 +4,27 @@ function playSound (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
   
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
-  console.log(key)
+
   //users presses unrecognised key
   if (!audio) return; 
   
   key.classList.add('playing')
+
+  //rewind to the start
   audio.currentTime = 0;
   audio.play();
 };
 
+function removeTransition (e) {
+  if (e.propertyName !== 'transform') return; //ignores all properties that are not tranform
+  e.target.classList.remove('playing');
+}
+
+//loops thorough the nodelist of keys to listen for the end of transition property
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound); 
-
 
 
 
