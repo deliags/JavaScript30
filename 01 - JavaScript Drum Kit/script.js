ad982a1615046c43db969e92d77f20eb8d8d22c4
deliags <deliagsmith@outlook.com>
2022-02-16 20:00:24 +0000

    created playSound function

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,9 +1,20 @@
-window.addEventListener('keydown', (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) return;
+function playSound (e) {
+  
+  //grabs the audio tag with the pressed keyCode
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+  
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+  console.log(key)
+  //users presses unrecognised key
+  if (!audio) return; 
+  
+  key.classList.add('playing')
   audio.currentTime = 0;
   audio.play();
-}); 
+};
+
+
+window.addEventListener('keydown', playSound); 
 
 
 
